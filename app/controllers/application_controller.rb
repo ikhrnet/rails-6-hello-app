4712d27ebf9359ee5c266ceaa4c902b7ba6905d7
ikhrnet <ikhrnet@users.noreply.github.com>
2021-02-01 16:14:22 +0900

    Add hola action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "hello, world!"
   end
+  def holla
+    render html: "hola, mundo!"
+  end
   def goodbye
     render html: "goodbye, world!"
   end
